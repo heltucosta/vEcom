32409efdef7f06b9b0ef5826696e2c3730a871e7
helton.costa <helton.costa@hexgis.com>
2017-12-19 08:27:59 -0200

    atualizado para heroku'

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -14,10 +14,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print(STATIC_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vEcom',
+        'USER': 'vEcom',
+        'PASSWORD': 'vEcom12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
